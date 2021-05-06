6ce7d406fb861c1278acf7c1d0bc9034cf88e781
Olena.Kasiianenko <olena.kasiianenko@gmail.com>
2021-05-06 22:38:30 +0200

    Create a file about pizza

---
+++ javascript/javascript1/week1/pizza.js
@@ -0,0 +1,24 @@
+console.log("I love pizza");
+const favPizza = "Margarita";
+const pizzaPrice = 100;
+console.log(`New Pizza Order: ${favPizza}`);
+console.log(`The Price of the Pizza is: ${pizzaPrice}`);
+
+// 2part of exersice
+
+const NumPizza = 3;
+let strFamily;
+let totalPrice;
+totalPrice = (pizzaPrice * NumPizza);
+
+if (strFamily = true) {
+    strFamily = "family size";
+    totalPrice *= 2;
+    console.log(`New pizza order: ${NumPizza} ${strFamily} ${favPizza}. Total cost for the order is: ${totalPrice}`);
+}
+
+else {
+    strFamily = "usual size";
+    console.log(`New pizza order: ${NumPizza} ${strFamily} ${favPizza}. Total cost for the order is: ${totalPrice}`);
+}
+
