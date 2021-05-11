4a76e7c7d6fb9008c506a671a7750404eb52ae1d
Olena.Kasiianenko <olena.kasiianenko@gmail.com>
2021-05-11 13:08:04 +0200

    changed variable declaration

--- javascript/javascript1/week1/Goodboy.js
+++ javascript/javascript1/week1/Goodboy.js
@@ -1,6 +1,6 @@
 const dogYearOfBirth = 2021;
 const dogYearFuture = 2027;
-let dogYear = (dogYearFuture-dogYearOfBirth)*7;
+const dogYear = (dogYearFuture-dogYearOfBirth)*7;
 let shouldShowResultInDogYears = true;
 if (shouldShowResultInDogYears) {
     console.log(`Your dog will be ${dogYear} dog years old in ${dogYearFuture}`);
