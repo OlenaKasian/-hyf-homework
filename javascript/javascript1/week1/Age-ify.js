dbaa45bc1ade77be417055ef333e9d3a212789d3
Olena.Kasiianenko <olena.kasiianenko@gmail.com>
2021-05-11 12:59:36 +0200

    change variable declaration

--- javascript/javascript1/week1/Age-ify.js
+++ javascript/javascript1/week1/Age-ify.js
@@ -1,5 +1,5 @@
 const yearOfBirth = 1986;
 const yearFuture = 2027;
-let age = yearFuture - yearOfBirth;
+const age = yearFuture - yearOfBirth;
 console.log(`You will be ${age} ears old in ${yearFuture}`);
 
