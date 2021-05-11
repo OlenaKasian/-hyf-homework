4e89fab6ea3346c2ae300ddac79e3db9fdd63fa9
Olena.Kasiianenko <olena.kasiianenko@gmail.com>
2021-05-11 13:01:47 +0200

    changed name of variable'
    git status

--- javascript/javascript1/week1/Ez-Namey.js
+++ javascript/javascript1/week1/Ez-Namey.js
@@ -1,7 +1,6 @@
 const firstWords = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Stars', 'Sun'];
 const secondWords = ['Dog', 'Puppy','Turtle', 'Rabbit', 'Parrot', 'Cat', 'Kitten', 'Goldfish', 'Mouse', 'Hamster'];
-const FirstRandomNumber = Math.floor(Math.random() * 10) + 0;
-const SecondRandomNumber = Math.floor(Math.random() * 10) + 0;
-let startupName;
-startupName = firstWords[FirstRandomNumber] + secondWords[SecondRandomNumber];
+const firstRandomNumber = Math.floor(Math.random() * 10);
+const secondRandomNumber = Math.floor(Math.random() * 10);
+const startupName = firstWords[firstRandomNumber] + secondWords[secondRandomNumber];
 console.log(`The startup: "${startupName}" contains ${startupName.length} characters`);
