a8e3870e4881e76eb382a114d088e780e4a690fc
Olena.Kasiianenko <olena.kasiianenko@gmail.com>
2021-09-09 21:20:45 +0200

    homework React week1

---
+++ react/week1/first-react-homework/src/TodoList/todoItem.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function TodoItem() {
+const doItem = [
+        {
+          id: 1,
+          description: 'Get out of bed',
+          date: 'Wed Sep 13 2021',
+        },
+        {
+          id: 2,
+          description: 'Brush teeth',
+          date: 'Thu Sep 14 2021',
+        },
+        {
+          id: 3,
+          description: 'Eat breakfast',
+          date: 'Fri Sep 15 2021',
+        }
+      ];
+
+    return doItem;
+}
+
+export default TodoItem;
---
+++ react/week1/first-react-homework/src/TodoList/todoList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import TodoItem from './todoItem'
+
+function TodoList() {
+    return <div>
+        <ul>
+            {doItem.map(todo => <TodoListItem id={todo.id} description={todo.description} date={todo.date} />)}
+        </ul>
+    </div>
+}
+function TodoListItem(props) {
+    return <li>
+        <p>{props.id}. {props.description}, {props.date}</p>
+    </li>;
+}
+
+export default TodoList;
